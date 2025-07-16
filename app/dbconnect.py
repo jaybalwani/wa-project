6fade84eb0914b7f64c3feb7c042874aab71a6aa
Jay Balwani <jaybalwani007@gmail.com>
2025-07-16 00:02:55 -0700

    added env

--- app/dbconnect.py
+++ app/dbconnect.py
@@ -1,16 +1,19 @@
 import mysql.connector
 import os
 from flask import g, current_app
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def connection(**kwargs):
     # if "db" not in g:
     try:
         g.db = mysql.connector.connect(
-            host=current_app.config["DB_HOST"],
-            port=current_app.config["DB_PORT"],
-            user=current_app.config["DB_USER"],
-            password=current_app.config["DB_PASSWORD"],
-            database=current_app.config["DB_NAME"],
+            host=os.getenv("DB_HOST", "wa-project-db.cd00i02egs50.ap-south-1.rds.amazonaws.com"),
+            port=os.getenv("DB_PORT", 3306),
+            user=os.getenv("DB_USER", "admin"),
+            password=os.getenv("DB_PASS", "HotAirBalloon123!"),
+            database="projectv2",
         )
         if kwargs and kwargs.get('dictFlag', {}):
             cursor = g.db.cursor(dictionary=True)
